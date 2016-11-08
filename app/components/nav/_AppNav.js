e7dd87f8169c32d67c39443c84fa16e2e253781a
Vetere Remo <Remo.Vetere@axelspringer.ch>
2016-11-09 00:41:52 +0100

    Make it work - well with errors but we got a graphql backend!!

--- app/components/nav/_AppNav.js
+++ app/components/nav/_AppNav.js
@@ -5,40 +5,40 @@ import { NavList, NavItem } from './navs.js';
 import Logo from '../icons/logo.js';
 import _ from 'lodash';
 
-class AppNav extends  React.Component{
+class AppNav extends  React.Component {
 
-	render(){
-		let { items } = this.props.menu;
-		items = _.sortBy(items, 'order');
+	render() {
+		  let { items } = this.props.menu;
+		  items = _.sortBy(items, 'order');
 
-		return(
+		  return (
 			<NavList type="primary">
 				<NavItem>
 					<Link to='/'><Logo/></Link>
 				</NavItem>
 				{items.map( item => {
-					const { children, object_type, post_title, navitem } = item;
+					  const { children, object_type, post_title, navitem } = item;
 
-          let linkText = post_title.length > 0 ? post_title : item.navitem.post_title
-          let pathname = object_type === "page" ? `/${item.navitem.post_name}` : `/${object_type}/${item.navitem.post_name}`;
+  let linkText = post_title.length > 0 ? post_title : item.navitem.post_title;
+  let pathname = object_type === 'page' ? `/${item.navitem.post_name}` : `/${object_type}/${item.navitem.post_name}`;
 
-					return(
+					  return (
 						<NavItem key={item.id}>
 							<Link to={{ pathname: pathname }} className={styles.link}>{linkText}</Link>
 							{children.length > 0 &&
 								<NavList type="subnav">
 									{children.map( child => {
-										return(
+										  return (
 											<NavItem type="link" href="{child.navitem.post_name}">{child.navitem.post_title}</NavItem>
-										)
+										);
 									})}
 								</NavList>
 							}
 						</NavItem>
-					)
+					);
 				})}
 			</NavList>
-		)
+		);
 	}
 }
 
--- app/components/nav/navs.js
+++ app/components/nav/navs.js
@@ -4,35 +4,35 @@ import styles from './navs.scss';
 
 const NavList = (props) =>{
 
-	let navClassName = styles[props.type];
+	    let navClassName = styles[props.type];
 
-	return (
+	    return (
 		<nav className={navClassName}>
 			<ul className={styles.list}>
 				{props.children}
 			</ul>
 		</nav>
-	)
+	);
 };
 
 const NavItem = (props) =>{
 
-	if (props.type =="link"){
-		return (
+	    if (props.type ==='link') {
+		    return (
 			<li>
 				<Link to={{ pathname: props.href }} onClick={props.onClick}>{props.children}</Link>
 			</li>
-		)
+		);
 	} else {
-		return (
+		    return (
 			<li className={styles.item}>
 				{props.children}
 			</li>
-		)
+		);
 	}
-}
+};
 
 let Navs = [];
 
 
-export { NavList, NavList, NavItem, Navs}
+export { NavList, NavItem, Navs};
